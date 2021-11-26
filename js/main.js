1bb565f0214dfbd2464158159d8aef2323ec0236
mkonovalov <mkonovalov@griddynamics.com>
2021-11-26 16:07:56 +0200

    create search utility for incidents tables

---
+++ js/main.js
@@ -0,0 +1,61 @@
+const search = document.getElementById("search");
+const matchList = document.getElementById("match-list");
+
+const searchStates = async (searchText) => {
+  const res = await fetch("data/data.json");
+  const incidents = await res.json();
+
+  let matches = incidents.filter((incident) => {
+    const regex = new RegExp(`^${searchText}`, "gi");
+    return incident.name.match(regex) || incident.thread.match(regex);
+  });
+
+  if (searchText.length === 0) {
+    matches = [];
+    const allIncidents = incidents
+      .map(
+        (incident) => `
+      <tr>
+        <td>${incident.number}</td>
+        <td>${incident.name}</td>
+        <td>${incident.owner}</td>
+        <td>${incident.location}</td>
+        <td>${incident.active}</td>
+        <td>${incident.thread}</td>
+        <td>${incident.threadSource}</td>
+        <td>${incident.threadMechanism}</td>
+      </tr>
+    `
+      )
+      .join("");
+    matchList.innerHTML = allIncidents;
+  }
+
+  outputHTML(matches);
+};
+
+const outputHTML = (matches) => {
+  if (matches.length > 0) {
+    const html = matches
+      .map(
+        (incident) => `
+        <tr>
+          <td>${incident.number}</td>
+          <td>${incident.name}</td>
+          <td>${incident.owner}</td>
+          <td>${incident.location}</td>
+          <td>${incident.active}</td>
+          <td>${incident.thread}</td>
+          <td>${incident.threadSource}</td>
+          <td>${incident.threadMechanism}</td>
+        </tr>
+      `
+      )
+      .join("");
+
+    matchList.innerHTML = html;
+  }
+};
+searchStates("");
+
+search.addEventListener("input", () => searchStates(search.value));
